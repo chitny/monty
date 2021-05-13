d42a9acc040c8ed4e627ac47c4d98b4b379b169f
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-13 21:49:14 +0000

    fixing the checks

--- checkfunc.c
+++ checkfunc.c
@@ -18,7 +18,17 @@ void (*get_op_func(line_t line, meta_t *meta))(stack_t **, unsigned int)
 		{"pop", pop},
 		{"swap", swap},
 		{"add", addop},
+		{"sub", subop},
+		{"div", divop},
+		{"mul", mulop},
+		{"mod", modop},
 		{"nop", nop},
+		{"pchar", pchar},
+		{"pstr", pstr},
+		{"rotl", rotlop},
+		{"rotr", rotrop},
+		{"stack", addst},
+		{"queue", addqu},
 		{NULL, NULL}};
 
 	if (comment_check(line))
